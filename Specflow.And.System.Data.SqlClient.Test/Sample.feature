6c1a0e13d795bb483ab6488beba0b748b580b99b
Bas Hamer <github@bashamer.com>
2020-01-09 19:54:36 -0700

    bug reproduction
    reproduction

---
+++ Specflow.And.System.Data.SqlClient.Test/Sample.feature
@@ -0,0 +1,4 @@
+﻿Feature: Test
+
+Scenario: DB
+Given bob
---
+++ Specflow.And.System.Data.SqlClient/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Specflow.And.System.Data.SqlClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection("bob");
+        }
+    }
+}
